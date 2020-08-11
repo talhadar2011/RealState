82171f87691ca0ef97430ddc45c14c7dc40a5d40
Your Name <talhadar>
2020-08-11 16:42:39 +0200

    Updates

---
+++ precache-manifest.793c8995fbccd8be0512221f4df2416c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c68e595c6ad2ecfbf0db685dd6df7f",
+    "url": "/RealState/index.html"
+  },
+  {
+    "revision": "f4ec1b1f228bb26f7aa2",
+    "url": "/RealState/static/js/2.54250a21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RealState/static/js/2.54250a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3994966254e7283f67",
+    "url": "/RealState/static/js/main.d56c34b3.chunk.js"
+  },
+  {
+    "revision": "de82e596dfd4c25abb89",
+    "url": "/RealState/static/js/runtime-main.edaf2c5f.js"
+  }
+]);
